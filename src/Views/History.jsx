9f89c2c0bacc02490ec4813d7b9f4760c16178f3
margaritaritik <margaritakuryaeva@gmail.com>
2023-12-12 17:45:51 +0700

    add information in history

--- src/Views/History.jsx
+++ src/Views/History.jsx
@@ -1,15 +1,37 @@
 import React from 'react';
-import styles from "./main.module.css";
+import styles from "./history.module.css";
 import Navbar from "../components/Navbar/Navbar";
 import Coffee from "../components/animations/coffee_beans/CoffeeBeans";
 import Footer from "../components/footer/footer";
+import coffeeBack from "../images/history/cup.jpg";
+import history1 from "../images/history/h1.png";
 
 const History = () => {
     return (
         <>
-            <div className={styles.App}>
+
                 <div className={styles.wrapper}>
-                    <Navbar></Navbar>
+                    <div className={styles.container_block}>
+                        <div className={styles.history}>
+                            <img className={styles.layer} src={coffeeBack}/>
+                            {/*<div>HISTORY</div>*/}
+                            {/*<div>COFFEE</div>*/}
+                            <div className={styles.title_block}>
+                                <p>история</p>
+                            </div>
+                            <div className={styles.title_block_coffee}>
+                                <p>кофе</p>
+                            </div>
+                            <div className={styles.block}>
+                                <img src={history1}/>
+                            </div>
+
+                        </div>
+                        {/*<div id={styles.test}></div>*/}
+
+                        <Navbar></Navbar>
+
+                    </div>
                     <div className={styles.content}>
                         <h1>ИСТОРИЯ</h1>
                         <p>Кофе был открыт в древней Эфиопии, когда пастух по имени Кальди заметил, что его овцы стали более активными и энергичными после того, как они поели ягоды кофейного дерева. Кальди решил попробовать ягоды сам и заметил, что они имеют стимулирующий эффект на организм. С тех пор кофе стал популярным напитком среди жителей Эфиопии и распространился по всему миру.
@@ -28,7 +50,7 @@ const History = () => {
 
                     <Footer></Footer>
                 </div>
-            </div>
+
 
         </>
     );
