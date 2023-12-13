5fdf4564d23e2e59f72640104f4002a8965d8aee
margaritaritik <margaritakuryaeva@gmail.com>
2023-12-13 22:27:36 +0700

    add information in statistika

--- src/Views/Statistika.jsx
+++ src/Views/Statistika.jsx
@@ -1,15 +1,26 @@
 import React from 'react';
-import styles from "./main.module.css";
+import styles from "./statistika.module.css";
 import Navbar from "../components/Navbar/Navbar";
 import Coffee from "../components/animations/coffee_beans/CoffeeBeans";
 import Footer from "../components/footer/footer";
+import coffee_map from "../images/statistika/map.png"
 
 const Statistika = () => {
     return (
         <>
             <div className={styles.App}>
                 <div className={styles.wrapper}>
-                    <Navbar></Navbar>
+                    <div className={styles.container_block}>
+                        <div className={styles.layer}>
+                            <section className={styles.stage}>
+                                <figure className={styles.ball}><span className={styles.shadow}></span></figure>
+                            </section>
+                            {/*<img src={coffee_map}/>*/}
+                        </div>
+
+                        <Navbar></Navbar>
+                    </div>
+
                     <div className={styles.content}>
                         <h1>СТАТИСТИКА КОФЕ</h1>
                         {/*<Header></Header>*/}
