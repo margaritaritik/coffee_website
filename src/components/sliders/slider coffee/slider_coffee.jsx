bb09e1d5ce9505f5a212b6c59a90c437a8814031
margaritaritik <margaritakuryaeva@gmail.com>
2023-12-14 20:57:25 +0700

    slider on main view

--- src/components/sliders/slider coffee/slider_coffee.jsx
+++ src/components/sliders/slider coffee/slider_coffee.jsx
@@ -4,6 +4,8 @@ import layer1 from '../../../images/facts/f1.png'
 import latte from '../../../images/slider/coffee/latte.png'
 import caputino from '../../../images/slider/coffee/caputino.png'
 import espresso from '../../../images/slider/coffee/espresso.png'
+import glasse from '../../../images/slider/coffee/glasse.png'
+import frape from '../../../images/slider/coffee/frape.png'
 
 const SliderCoffee = () => {
     const [slideId, setSlideId] = useState(1);
@@ -16,92 +18,51 @@ const SliderCoffee = () => {
     const slider=[
         {
             id:1,
-            name:"ЛАТТЭ",
+            name:"Латте",
             style_img:styles.latte,
             style_back:styles.layer_latte,
             image:latte
         },
         {
             id:2,
-            name:"Капучино",
+            name:"Каппучино",
             style_img:styles.caputino,
             style_back:styles.layer_caputino,
             image:caputino
         },
         {
             id:3,
-            name:"ЭСПРЕССО",
+            name:"Гляссе",
+            style_img:styles.glasse,
+            style_back:styles.layer_glasse,
+            image:glasse
+        },
+        {
+            id:4,
+            name:"Фраппе",
+            style_img:styles.frape,
+            style_back:styles.layer_frape,
+            image:frape
+        },
+        {
+            id:5,
+            name:"Эспрессо",
             style_img:styles.espresso,
             style_back:styles.layer_espresso,
             image:espresso
-        },
-        {
-            id:1,
-            name:"ЛАТТЭ",
-            style_img:styles.latte,
-            style_back:styles.layer_latte,
-            image:latte
-        },
-        {
-            id:3,
-            name:"ЭСПРЕССО",
-            style_img:styles.espresso,
-            style_back:styles.layer_espresso,
-            image:espresso
-        },
-        {
-            id:1,
-            name:"ЛАТТЭ",
-            style_img:styles.latte,
-            style_back:styles.layer_latte,
-            image:latte
-        },
-        {
-            id:3,
-            name:"ЭСПРЕССО",
-            style_img:styles.espresso,
-            style_back:styles.layer_espresso,
-            image:espresso
-        },
-        {
-            id:1,
-            name:"ЛАТТЭ",
-            style_img:styles.latte,
-            style_back:styles.layer_latte,
-            image:latte
         }
 
     ];
 
-    // const listItems =slider.map(slide =>
-    //     <div className={styles.block}>
-    //         <div className={styles.layer1}>
-    //             <p>{slide.name}</p>
-    //         </div>
-    //         <div className={styles.layer2}>
-    //             <img className={slide.style_back} src={layer1}/>
-    //         </div>
-    //         <div className={styles.layer3}>
-    //             <img className={slide.style_img} src={slide.image}/>
-    //         </div>
-    //     </div>
-    // );
     const listItems =slider.map(slide =>
         <div className={styles.block}>
             <div className={styles.layer1}>
                 <p>{slide.name}</p>
-                {/*<div className={styles.layer2}>*/}
                 <div className={styles.layer2}>
                     <img className={slide.style_back} src={layer1}/>
                     <img className={slide.style_img} src={slide.image}/>
                 </div>
-
-                {/*</div>*/}
             </div>
-
-            {/*<div className={styles.layer3}>*/}
-            {/*   */}
-            {/*</div>*/}
         </div>
     );
 
